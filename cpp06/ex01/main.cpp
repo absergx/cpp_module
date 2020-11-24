3b09b633491b047447684beedb0ded9e29d368f1
Meldred Emilio <memilio@si-g1.kzn.21-school.ru>
2020-11-24 15:59:48 +0300

    randomizer added

--- cpp06/ex01/main.cpp
+++ cpp06/ex01/main.cpp
@@ -5,15 +5,21 @@
 #include <string>
 #include <iostream>
 
-struct Data {
-	std::string	s1;
-	int			n;
-	std::string	s2;
-};
+//struct Data {
+//	std::string	s1;
+//	int			n;
+//	std::string	s2;
+//};
+
+char randomChar() {
+	std::srand(time(0));
+	const std::string setOfChars = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+	return setOfChars[rand() % 62];
+}
 
 void * serialize(void);
 
 int main() {
-
+	std::cout << randomChar() << std::endl;
 	return 0;
 }
