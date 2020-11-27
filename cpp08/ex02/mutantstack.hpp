602cdab678fe1f7b7d7b1fc2f0ccbffe4beea17e
Meldred Emilio <memilio@si-g1.kzn.21-school.ru>
2020-11-27 20:28:07 +0300

    iterators need to add ++ and --

--- cpp08/ex02/mutantstack.hpp
+++ cpp08/ex02/mutantstack.hpp
@@ -12,7 +12,34 @@
 template<typename T>
 class MutantStack : public std::stack<T> {
 public:
-
+	MutantStack() : std::stack<T>() {};
+	virtual ~MutantStack() {};
+	MutantStack(const MutantStack &other) : std::stack<T>(other) {};
+	MutantStack&	operator=(const MutantStack &other) {
+		*this = other;
+		return *this;
+	}
+class Iterator : public std::iterator<std::bidirectional_iterator_tag, T> {
+private:
+		T*		_pointer;
+public:
+		Iterator() : _pointer(nullptr) {};
+		Iterator(T *pointer) : _pointer(pointer) {};
+		Iterator(MutantStack<T>::Iterator const &iterator) {*this = iterator;};
+		~Iterator() {};
+		MutantStack<T>::Iterator&	operator=(const MutantStack<T>::Iterator& other) {
+			if (this != &other)
+				this->_pointer = other._pointer;
+			return *this;
+		}
+		// operator++
+		// &operator++
+		// operator--
+		// &operator--
+		T&	operator*() {return *(this->_pointer);};
+	};
+	MutantStack<T>::Iterator	begin() {return Iterator(&this->top());}
+	MutantStack<T>::Iterator	end() {return Iterator(&this->top() - this->size());};
 };
 
 #endif //CPP08_TMP_MUTANTSTACK_HPP
